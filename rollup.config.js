c4a24bec74def7234dc32c2f138054fd56d9362b
flejz <flejz@protonmail.com>
2026-05-30 21:19:19 +0200

    build(proxmox-card): output bundle to repo root for HACS compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import terser from '@rollup/plugin-terser';
 export default {
   input: 'src/proxmox-card.js',
   output: {
-    file: 'dist/proxmox-card.js',
+    file: 'proxmox-card.js',
     format: 'es',
     sourcemap: false,
   },
